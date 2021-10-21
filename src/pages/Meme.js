a0d5237440a0123eb36490c22ca4cb6d545cd4b1
Joshua Adesanya <adesanyajoshua@ymail.com>
2021-10-21 16:19:32 +0100

    changes

--- src/pages/Meme.js
+++ src/pages/Meme.js
@@ -147,7 +147,7 @@ export default function Meme() {
 
     const postImage = async (userCred) => {
         try {
-            const res = await axios.patch(`http://localhost:8081/api/v1/image`, userCred);
+            const res = await axios.patch(`https://checkspecstatus.com/api/v1/image`, userCred);
             console.log(res);
             return res.data;
         } catch (err) {
@@ -157,7 +157,7 @@ export default function Meme() {
 
     const postImage2 = async (userCred) => {
         try {
-            const res = await axios.patch(`http://localhost:8081/api/v1/image/finish`, userCred);
+            const res = await axios.patch(`https://checkspecstatus.com/api/v1/image/finish`, userCred);
             console.log(res);
             return res.data;
         } catch (err) {
